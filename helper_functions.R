0dc90bea311bce82a3343e6de703ce8c7a371bc3
annariha <e.riha+git@posteo.de>
2022-05-08 13:22:03 +0300

    compare posteriors of one, several or list of fit-objects and exclude vars from plot

--- helper_functions.R
+++ helper_functions.R
@@ -2,31 +2,48 @@
 mean_impute <- function(a) ifelse(is.na(a), mean(a[!is.na(a)]), a)
 standardize <- function(a) (a - mean(a))/(2*sd(a))
 
-# compare posteriors visually
-# code slightly adapted from https://stackoverflow.com/questions/52875665/plotting-posterior-parameter-estimates-from-multiple-models-with-bayesplot
-# new: input can be a list of brmsfit-objects 
+# compare posteriors of several models visually
+# code adapted from https://stackoverflow.com/questions/52875665/plotting-posterior-parameter-estimates-from-multiple-models-with-bayesplot
+# new: input can be one, more or a list of fit-objects, option to exclude vars from plot
 
-compare_posteriors <- function(..., dodge_width = 0.5) {
-  dots <- rlang::dots_list(..., .named = TRUE)
-  draws <- lapply(dots, function(x) {
+compare_posteriors <- function(x, ... , dropvars = c(), dodge_width = 0.5) {
+  # put "dots" in list 
+  dots <- rlang::dots_list(..., .named = TRUE) 
+  # coerce if x is one fit-object and more objects are in "dots"
+  # otherwise take list of fit-objects 
+  if (class(x) != "list"){
+    fits <- c(list(x), dots) 
+  } else {
+    fits <- x
+  }
+  
+  # process fit-objects to get draw-arrays 
+  draws <- lapply(fits, function(x) {
     if (class(x)[1] == "stanreg") {
       posterior::subset_draws(posterior::as_draws(x$stanfit),
                               variable = names(fixef(x))
       )
     } else if (class(x)[1] == "brmsfit") {
       brm_draws <- posterior::subset_draws(posterior::as_draws(x$fit),
-                                           variable = paste0("b_", rownames(fixef(x)))
-      )
+                                           variable = paste0("b_", rownames(fixef(x))))
       posterior::variables(brm_draws) <- stringr::str_split(posterior::variables(brm_draws), "_", simplify = T)[, 2]
       posterior::rename_variables(brm_draws, `(Intercept)` = Intercept)
     } else {
       stop(paste0(class(x)[1], " objects not supported."))
     }
   })
+  
   intervals <- lapply(draws, bayesplot::mcmc_intervals_data)
-  names(intervals) <- 1:length(intervals) # 1, 2, 3 etc but names could also be sth else
+  # names: 1,2,3,... but names could be sth. more descriptive
+  names(intervals) <- 1:length(intervals) 
   combined <- dplyr::bind_rows(intervals, .id = "model") %>%
     mutate(model = factor(model, levels = unique(as.character(model)))) # get proper factors from list names
+  
+  # option to exclude vars
+  combined <- combined %>%
+       filter(!parameter %in% dropvars)
+  
+  # plot
   ggplot(combined, aes(x = m, y = parameter, color = model, group = model)) +
     geom_linerange(aes(xmin = l, xmax = h), size = 2, position = position_dodge(dodge_width)) +
     geom_linerange(aes(xmin = ll, xmax = hh), position = position_dodge(dodge_width)) +
