0e9dca355f55afba8763d8bacebf1fe858c89287
Riha Anna <rihaa1@t31300-lr031.org.aalto.fi>
2022-06-29 11:49:35 +0300

    extract diags

--- helper_functions.R
+++ helper_functions.R
@@ -13,6 +13,83 @@ extract_vars_df <- function (x, ..., .results = .results){
     select(-extracted)
 }
 
+# extract convergence diagnostics
+check_rhats <- function(named_vec){
+  # from https://mc-stan.org/misc/warnings.html; # "only fully trust the sample if R-hat is less than 1.01"
+  # > 1.05 e.g. 3/5 parameters without convergence issues - a, c had issues 
+  out <- names(named_vec[named_vec > 1.01])
+  return(out)
+}
+
+# "check also Monte Carlo standard error for the quantities of interest and 
+# compare that to the domain knowledge of the required accuracy, and if it is 
+# low, then run longer chains to get more samples from the posterior."
+
+# extract the number of divergent transitions
+# "if you get only few divergences and you get good Rhat and ESS values, 
+# the resulting posterior is often good enough to move forward"
+extract_div_trans <- function(df){
+  # from brms documentation
+  sum(subset(df, Parameter == "divergent__")$Value)
+}
+
+# "effective sample size (ESS) of a quantity of interest captures how many 
+# independent draws contain the same amount of information as the dependent 
+# sample obtained by the MCMC algorithm. The higher the ESS the better. [...]
+# For final results, we recommend requiring that the bulk-ESS is greater than 
+# 100 times the number of chains. For example, when running four chains, this 
+# corresponds to having a rank-normalized effective sample size of at least 400. 
+# In early workflow, ESS > 20 is often sufficient." 
+
+check_bulkess <- function(modelfit){
+  nc <- nchains(modelfit)
+  bulkess <- posterior::ess_bulk(modelfit)
+  # flag if bulk ESS < 100 * number of chains
+  out <- bulkess[bulkess < 100 * nc] 
+}
+
+evaluate_multiverse <- function(multiverse, mod, outcome){
+  # to pass column names of multiverse object without quotation marks
+  mod <- deparse(substitute(mod))
+  # evaluate and extract 
+  m_dict <- multiverse %>%
+    mutate(rhats = purrr::map(multi[[mod]], brms::rhat), 
+           flagrhats = purrr::map(purrr::map(multi[[mod]], brms::rhat), check_rhats),
+           flag_rhats = purrr::map_dbl(flagrhats, length), 
+           neffs = purrr::map(multi[[mod]], brms::neff_ratio),
+           divtrans = purrr::map_dbl(purrr::map(multi[[mod]], brms::nuts_params), extract_div_trans),
+           bulkess = purrr::map_dbl(multi[[mod]], posterior::ess_bulk),
+           flagbulkess = purrr::map(multi[[mod]], check_bulkess),
+           flag_bulkess = if_else(purrr::map_dbl(multi[[mod]], posterior::ess_bulk) < 100 * 4, "too low", "ok"),
+           # priorsense
+           ps_df_params = purrr::map(purrr::map(multi[[mod]], priorsense::powerscale_sensitivity), "sensitivity"),
+           ps_params = purrr::map(ps_df_params, ~filter(.x, diagnosis != "-")),
+           pdc = purrr::map(purrr::map(ps_df_params, ~filter(.x, diagnosis == "prior-data conflict")), count) %>% unlist(),
+           # PPC
+           yrep = purrr::map(multi[[mod]], posterior_predict),
+           mean_yrep = purrr::map(yrep, colMeans),
+           dist_ws = purrr::map(mean_yrep, wasserstein1d, a = outcome) %>% unlist(), # this line needs data
+           # loo 
+           results_loo = purrr::map(multi[[mod]], loo),
+           flag_pareto_ks = purrr::map_dbl(purrr::map(purrr::map(loo_results, "diagnostics"), "pareto_k"), ~sum(.x > 0.7)),
+           elpd_loo = purrr::map_dbl(purrr::map(results_loo, "estimates"), 1),
+           se_elpd_loo = purrr::map_dbl(purrr::map(results_loo, "estimates"), 4),
+           p_loo = purrr::map_dbl(purrr::map(results_loo, "estimates"), 2),
+           se_p_loo = purrr::map_dbl(purrr::map(results_loo, "estimates"), 5)) %>%
+    select(!c(.parameter_assignment, 
+              .code, 
+              .results, 
+              mod, 
+              rhats, 
+              neffs, 
+              ps_df_params, 
+              ps_params, 
+              yrep, 
+              mean_yrep,
+              results_loo))
+  out <- m_dict
+}
+
 # compare posteriors of several models visually
 # code adapted from https://stackoverflow.com/questions/52875665/plotting-posterior-parameter-estimates-from-multiple-models-with-bayesplot
 # new: input can be one, more or a list of fit-objects, option to exclude vars from plot
@@ -61,3 +138,4 @@ compare_posteriors <- function(x, ... , dropvars = c(), dodge_width = 0.5) {
     geom_point(color = "black", position = position_dodge(dodge_width), size = 0.8) +
     geom_vline(xintercept = 0, linetype = "dashed")
 }
+
