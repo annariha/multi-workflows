4b33d59a2a032b42fb427360e3d211154d24869f
annariha <rihaa1@t31300-lr031.org.aalto.fi>
2023-02-28 18:46:01 +0200

    init

---
+++ case-studies/epilepsy/R/get_rhat_trt.R
@@ -0,0 +1,7 @@
+# check Rhat for "b_Trt1" (and "b_zBase:Trt1" if present)
+get_rhat_trt <- function(modelfit){
+  rhats <- brms::rhat(modelfit)
+  # find "b_Trt1" (and if present "b_zBase:Trt1")
+  rhat_trt <- rhats[str_detect(names(rhats), regex("Trt1", ignore_case = TRUE))]
+  return(rhat_trt)
+}
