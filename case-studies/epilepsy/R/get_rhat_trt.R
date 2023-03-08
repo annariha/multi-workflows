a85d5b56a2c36cd46b86ccfd45d050d2908f7fab
annariha <rihaa1@t31300-lr031.org.aalto.fi>
2023-03-08 18:26:05 +0200

    return rhat as numeric

--- case-studies/epilepsy/R/get_rhat_trt.R
+++ case-studies/epilepsy/R/get_rhat_trt.R
@@ -3,5 +3,6 @@ get_rhat_trt <- function(modelfit){
   rhats <- brms::rhat(modelfit)
   # find "b_Trt1" (and if present "b_zBase:Trt1")
   rhat_trt <- rhats[str_detect(names(rhats), regex("Trt1", ignore_case = TRUE))]
+  rhat_trt <- as.numeric(rhat_trt[1])
   return(rhat_trt)
 }
