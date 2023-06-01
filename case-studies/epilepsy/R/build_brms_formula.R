0e38640a59f65ed940107b09ec30f1804fb32d24
annariha <rihaa1@t31300-lr031.org.aalto.fi>
2023-06-01 12:13:41 +0300

    fixed typo

--- case-studies/epilepsy/R/build_brms_formula.R
+++ case-studies/epilepsy/R/build_brms_formula.R
@@ -2,7 +2,7 @@
 build_brms_formula <- function(row, ...){
   outcome = row[["outcome"]]
   fam = as.character(unlist(row["family"]))
-  # which cells in the row are not "family", "model_name" and non-empty?
+  # which cells in the row are not "family", "model_name" etc. and non-empty?
   in_id <- c(which(!(names(row) %in% c("outcome", "family", "prior", "priors", "model_name")) & row != ""))
   # cells that are included in the formula
   covars <- row[in_id]
