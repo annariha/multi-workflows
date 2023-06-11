eba779aa7f9c823632bfa24f00502e7f9c2b5412
annariha <rihaa1@t31300-lr031.org.aalto.fi>
2023-06-11 16:22:22 +0300

    store to not recompute (for now)

--- case-studies/epilepsy/R/build_loo.R
+++ case-studies/epilepsy/R/build_loo.R
@@ -1,16 +1,20 @@
+################################################################################
 # loo: elpd and model comparison ####
+# if observation level random effect, then integrated loo
 
 build_loo <- function(row, ...){
   # print(build_name(row))
-  file_name = paste0(digest::digest(build_name(row), algo="md5"), "_loo.rds")
+  file_name = paste0("case-studies", "epilepsy", "data", "prelim", digest::digest(build_name(row), algo="md5"), "_loo.rds")
   if(file.exists(file_name)){
     return(readRDS(file_name))
-  }else{
+  } else {
     rv = loo(build_fit(row), model_names=c(build_name(row)))
     saveRDS(rv, file_name)
     return(rv)
   }
 } 
+
+################################################################################
 
 build_loo_rstan <- function(row, ...){
   file_name = paste0(digest::digest(build_name(row), algo="md5"), "_loo_rstan.rds")
