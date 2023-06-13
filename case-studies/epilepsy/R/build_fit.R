3502aca3d4e962b164e20a4bd3077150bda9d724
annariha <rihaa1@t31300-lr031.org.aalto.fi>
2023-06-13 17:05:16 +0300

    create dir if not exists, set seed

--- case-studies/epilepsy/R/build_fit.R
+++ case-studies/epilepsy/R/build_fit.R
@@ -1,16 +1,24 @@
 # fit model for each combination ####
 
-build_fit <- function(row, ...){
+build_fit <- function(row, dataset, ...){
+  # for storing results
+  filedir = here::here("case-studies", "epilepsy", "data", "prelim", "modelfits")
+  if (!dir.exists(filedir)) {dir.create(filedir)}
+  # fit model with brms
   brm(
     formula = build_brms_formula(row), 
     data = dataset, 
     prior = row[["prior"]],
-    file = here::here("case-studies", "epilepsy", "data", "prelim", digest::digest(build_name(row), algo="md5")),
+    seed = 424242,
+    file = here::here("case-studies", "epilepsy", "data", "prelim", "modelfits", digest::digest(build_name(row), algo="md5")),
     backend = "cmdstanr", 
     silent = 2, 
     refresh = 0
   ) 
 }
+
+#test <- combinations_df[3,]
+#test_fit <- build_fit(test, dataset = brms::epilepsy)
 
 build_fit_rstan <- function(row, ...){
   brm(
