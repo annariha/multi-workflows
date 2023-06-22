ee38972974f210c958bb400299a380555904cdce
Anna Elisabeth Riha <e.riha@posteo.de>
2023-06-22 17:58:56 +0300

    for testing

--- case-studies/epilepsy/R/build_loo.R
+++ case-studies/epilepsy/R/build_loo.R
@@ -4,14 +4,14 @@
 
 build_loo <- function(row, dataset, ...){
   # print(build_name(row))
-  file_name = paste0("case-studies", "epilepsy", "data", "prelim", digest::digest(build_name(row), algo="md5"), "_loo.rds")
-  if(file.exists(file_name)){
-    return(readRDS(file_name))
-  } else {
+  #file_name = paste0(digest::digest(build_name(row), algo="md5"), "_loo.rds")
+  #if(file.exists(file_name)){
+    #return(readRDS(file_name))
+  #} else {
     rv = loo(build_fit(row, dataset), model_names=c(build_name(row)))
-    saveRDS(rv, file_name)
+    #write_rds(rv, file_name)
     return(rv)
-  }
+  #}
 } 
 
 ################################################################################
