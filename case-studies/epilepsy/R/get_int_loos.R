b545e09e8df64f2fe02b7be9a666dd8f261775fa
Anna Elisabeth Riha <e.riha@posteo.de>
2023-06-22 17:59:55 +0300

    add modelnames for comparison df

--- case-studies/epilepsy/R/get_int_loos.R
+++ case-studies/epilepsy/R/get_int_loos.R
@@ -22,12 +22,20 @@ source(here::here("case-studies", "epilepsy", "R", "build_loglik.R"))
 # run script to get combinations dataframe
 source(here::here("case-studies", "epilepsy", "R", "get_combinations.R"))
 
+# store modelnames for later use in loo()####
+modelnames <- combinations_df |> 
+  # only for models with obs-level random intercept
+  filter(obs != "") |>
+  group_nest(row_number()) |>
+  pull(data) |>
+  purrr::map_chr(~build_name(.x))
+
 # loo objects with integration ####
 tic()
 future::plan(multisession, workers = parallel::detectCores() - 2)
 loos_obs_randint <- combinations_df |> 
   # only for models with obs-level random intercept
-  filter(obs != "") |> 
+  filter(obs != "") |>
   # for testing, 317.631 sec elapsed
   # slice_sample(n = 2) |>
   group_nest(row_number()) |>
@@ -35,6 +43,18 @@ loos_obs_randint <- combinations_df |>
   furrr::future_map(~build_loglik_2(.x, dataset = brms::epilepsy), .options=furrr_options(seed=TRUE)) |>
   furrr::future_map(~loo::loo(.x, r_eff = loo::relative_eff(exp(.x))), .options=furrr_options(seed=TRUE))
 toc()
+
+# add model names
+names(loos_obs_randint) <- modelnames
+
+# join with default loos for models without obs-level random intercept
+loos_with_default <- read_rds(here::here("case-studies", "epilepsy", "results", "loos_with_default.rds"))
+modelnames_without_obs_randint <- setdiff(names(loos_with_default), names(loos_obs_randint))
+loos_without_obs_randint <- loos_with_default[modelnames_without_obs_randint]
+loos_all <- c(loos_obs_randint, loos_without_obs_randint)
+
+# compare models with loo & model averaging weights ####
+comparison_df_randint = loo::loo_compare(loos_all)
 
 # store results ####
 filedir = here::here("case-studies", "epilepsy", "results")
