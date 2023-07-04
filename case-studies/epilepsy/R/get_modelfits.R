97f51b51a88d46dbcc2338d01b51386a672d0070
Anna Elisabeth Riha <e.riha@posteo.de>
2023-07-04 17:08:56 +0300

    add draws df

--- case-studies/epilepsy/R/get_modelfits.R
+++ case-studies/epilepsy/R/get_modelfits.R
@@ -3,8 +3,7 @@
 # setup ####
 # load packages 
 if(!requireNamespace("pacman")) install.packages("pacman")
-pacman::p_load(here, tictoc, future, furrr, purrr, parallel, 
-               brms, Matrix, tidyverse, cmdstanr, spatstat)
+pacman::p_load(here, tictoc, future, purrr, parallel, brms, Matrix, tidyverse, cmdstanr)
 
 # set seed
 set.seed(42424242)
@@ -25,6 +24,14 @@ source(here::here("case-studies", "epilepsy", "R", "get_combinations.R"))
 models_combs_df <- combinations_df |>
   mutate(modelnames = apply(combinations_df, 1, build_name))
 
+# if needed: save stancode for each model ####
+#helper_data <- combinations_df |>
+#  mutate(formula = apply(combinations_df, 1, build_brms_formula)) |>
+#  select(formula, family, prior) |>
+#  mutate(df = list(brms::epilepsy)) |>
+#  mutate(save_model = here::here("case-studies", "epilepsy", "Stan", paste0("model_", 1:NROW(combinations_df), ".stan")))
+#purrr::pmap(helper_data, brms::make_stancode)
+
 # fit models ####
 tic()
 future::plan(multisession)
@@ -37,7 +44,11 @@ models_combs_df$modelfits <- combinations_df |>
                     .options=furrr_options(seed=TRUE))
 toc()
 
-# save modelfits ####
+# add draws df ####
+models_combs_df <- models_combs_df |>
+  mutate(draws_df = purrr::map(purrr::map(modelfits, pluck), posterior::as_draws_df))
+
+# save df with modelfits ####
 filedir = here::here("case-studies", "epilepsy", "results")
 if (!dir.exists(filedir)) {dir.create(filedir)}
 readr::write_rds(models_combs_df, here::here("case-studies", "epilepsy", "results", "models_combs_df.rds"))
