d5e3ed5ca9de5e46958871079dacde8bd86f4cf0
Anna Elisabeth Riha <e.riha@posteo.de>
2023-07-05 20:30:51 +0300

    set names, store results, reduced

--- case-studies/epilepsy/R/get_loos.R
+++ case-studies/epilepsy/R/get_loos.R
@@ -6,6 +6,10 @@ if(!requireNamespace("pacman")) install.packages("pacman")
 pacman::p_load(here, tictoc, future, furrr, purrr, parallel, 
                brms, Matrix, tidyverse, cmdstanr, spatstat)
 
+# check and create dir if needed
+filedir = here::here("case-studies", "epilepsy", "results")
+if (!dir.exists(filedir)) {dir.create(filedir)}
+
 # set seed
 set.seed(42424242)
 
@@ -14,48 +18,28 @@ nc <- detectCores() - 2
 options(mc.cores = nc) 
 
 # load functions
-source(here::here("case-studies", "epilepsy", "R", "build_name.R"))
-source(here::here("case-studies", "epilepsy", "R", "build_brms_formula.R"))
-source(here::here("case-studies", "epilepsy", "R", "build_fit.R"))
 source(here::here("case-studies", "epilepsy", "R", "build_loo.R"))
+source(here::here("case-studies", "epilepsy", "R", "build_loglik.R"))
 
-# run script to get combinations dataframe
-source(here::here("case-studies", "epilepsy", "R", "get_combinations.R"))
-
-# all modelfits ####
-#tic()
-#future::plan(multisession)
-#modelfits <- combinations_df |>
-#  group_nest(row_number()) |>
-#  pull(data) |>
-#  furrr::future_map(~build_fit(.x, dataset = brms::epilepsy), 
-#                    .options=furrr_options(seed=TRUE))
-#toc()
-#write_rds(modelfits, here::here("case-studies", "epilepsy", "results", "modelfits.rds"))
+# load modelfits 
+models_combs_df <- readr::read_rds(here::here("case-studies", "epilepsy", "results", "models_combs_df.rds"))
 
 # default loos for all models ####
+
+# for testing
+# models_combs_df <- models_combs_df |>
+# dplyr::slice_sample(n = 5)
+
 tic()
-future::plan(multisession)
-loos_with_default <- combinations_df |>
-  # for testing, 126.326 sec elapsed
-  # dplyr::slice_sample(n = 5) |>
+future::plan(multisession, workers = parallel::detectCores() - 2)
+loos_default <- models_combs_df |>
   group_nest(row_number()) |>
   pull(data) |>
-  furrr::future_map(~build_loo(.x, dataset = brms::epilepsy), .options=furrr_options(seed=TRUE))
+  furrr::future_map(~build_loos(.x, dataset = brms::epilepsy), .options=furrr_options(seed=TRUE))
 toc()
 
-# add modelnames 
-modelnames_all <- combinations_df |>
-  group_nest(row_number()) |>
-  pull(data) |>
-  purrr::map_chr(~build_name(.x))
+# set names for loo objects
+names(loos_default) <- models_combs_df$modelnames
 
-names(loos_with_default) <- modelnames_all
-
-# compare models with loo & model averaging weights ####
-comparison_df_default = loo::loo_compare(loos_with_default)
-
-# store results ####
-filedir = here::here("case-studies", "epilepsy", "results")
-if (!dir.exists(filedir)) {dir.create(filedir)}
-write_rds(loos_with_default, here::here("case-studies", "epilepsy", "results", "loos_with_default.rds"))
+# store intermediate results ####
+readr::write_rds(loos_default, here::here("case-studies", "epilepsy", "results", "loos_default.rds"))
