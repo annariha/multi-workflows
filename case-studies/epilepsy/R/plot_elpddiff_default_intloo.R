7db126178bce6cf5c0d825f15b19982abc65c2dc
Anna Elisabeth Riha <e.riha@posteo.de>
2023-07-06 17:33:35 +0300

    nicer plots, save tikz

--- case-studies/epilepsy/R/plot_elpddiff_default_intloo.R
+++ case-studies/epilepsy/R/plot_elpddiff_default_intloo.R
@@ -12,6 +12,9 @@ theme_set(theme_bw() +
                   strip.background = element_blank(),
                   panel.background = element_blank()))
 
+# load scripts
+source(here::here("case-studies", "epilepsy", "R", "save_tikz_plot.R"))
+
 # check and create dir if needed
 filedir = here::here("case-studies", "epilepsy", "figures")
 if (!dir.exists(filedir)) {dir.create(filedir)}
@@ -23,44 +26,62 @@ full_df_elpddiff_pbma <- readr::read_rds(here::here("case-studies", "epilepsy", 
 df_plot_elpddiff_default <- full_df_elpddiff_pbma |>
   mutate(modelnames = rownames(full_df_elpddiff_pbma)) |>
   filter(loo_computation == "default") |>
-  mutate(high_pareto_ks = ifelse(n_high_pareto_ks > (NROW(full_df_elpddiff_pbma) / 100) * 5, "yes", "no")) |>
+  mutate(high_pareto_ks = ifelse(n_high_pareto_ks > (NROW(brms::epilepsy) / 100) * 5, "yes", "no")) |>
   arrange(elpd_diff) |>
-  mutate(modelnames = forcats::fct_inorder(modelnames)) |>
-  select(modelnames, elpd_diff, se_diff, n_high_pareto_ks, high_pareto_ks, loo_computation)
+  mutate(model_id = forcats::fct_inorder(model_id)) |>
+  select(modelnames, elpd_diff, se_diff, n_high_pareto_ks, model_id, high_pareto_ks, loo_computation)
 
-plot_elpd_diff_default <- ggplot(data = df_plot_elpddiff_default, aes(elpd_diff, modelnames, col = high_pareto_ks)) +
+plot_elpd_diff_default <- ggplot(data = df_plot_elpddiff_default, aes(elpd_diff, model_id, col = high_pareto_ks)) +
   geom_pointrange(aes(xmin=elpd_diff-se_diff, xmax=elpd_diff+se_diff), shape=21) +
   geom_vline(xintercept = 0, linetype = "dashed") + 
-  scale_color_manual(values=c("black","red")) +
-  theme(axis.title.y = element_blank(),
-        #axis.text.y = element_text(size=15),
+  xlab("$Delta elpdPlain$") +
+  ylab("Models")+
+  scale_color_manual(values=c("black","red"), name="Too-high Pareto $hat k$") +
+  # change scales to sth that makes sense
+  scale_x_continuous(trans = "pseudo_log", breaks = c(0, -500, -1000, -2000)) +
+  theme(
+        axis.text.y = element_blank(),
         #axis.text.x = element_text(size=15)
-        #,legend.position = "none"
+        legend.position = c(0.2,0.9)
         )
 
-save_plot(here::here("case-studies", "epilepsy", "figures", "plot_elpd_diff_default.png"), 
-          plot_elpd_diff_default, 
-          base_height = 15, 
-          base_aspect_ratio = 1.5)
+plot_elpd_diff_default
+
+save_tikz_plot(plot = plot_elpd_diff_default, 
+               width = 6,
+               filename = here::here("case-studies", "epilepsy", "figures", "plot_elpd_diff_default.tex")
+               )
 
 # create plot of elpd diff +/- se diff for integrated LOO ####
 df_plot_elpddiff_intloo <- full_df_elpddiff_pbma |>
   mutate(modelnames = rownames(full_df_elpddiff_pbma)) |>
   filter(loo_computation == "integrated LOO") |>
-  mutate(high_pareto_ks = ifelse(n_high_pareto_ks > (NROW(full_df_elpddiff_pbma) / 100) * 5, "yes", "no")) |>
+  mutate(high_pareto_ks = ifelse(n_high_pareto_ks > (NROW(brms::epilepsy) / 100) * 5, "yes", "no")) |>
   arrange(elpd_diff) |>
-  mutate(modelnames = forcats::fct_inorder(modelnames)) |>
-  select(modelnames, elpd_diff, se_diff, n_high_pareto_ks, high_pareto_ks, loo_computation)
+  mutate(model_id = forcats::fct_inorder(model_id)) |>
+  select(modelnames, elpd_diff, se_diff, n_high_pareto_ks, model_id, high_pareto_ks, loo_computation)
 
-plot_elpd_diff_intloo <- ggplot(data = df_plot_elpddiff_intloo, aes(elpd_diff, modelnames, col = high_pareto_ks)) +
+plot_elpd_diff_intloo <- ggplot(data = df_plot_elpddiff_intloo, aes(elpd_diff, model_id, col = high_pareto_ks)) +
   geom_pointrange(aes(xmin=elpd_diff-se_diff, xmax=elpd_diff+se_diff), shape=21) +
   geom_vline(xintercept = 0, linetype = "dashed") + 
-  scale_color_manual(values=c("black","red")) +
-  theme(axis.title.y = element_blank(),
-        #axis.text.y = element_text(size=15),
-        #axis.text.x = element_text(size=15)
-        #,legend.position = "none"
+  xlab("$Delta elpdPlain$") +
+  ylab("Models")+
+  scale_color_manual(values=c("black","red"), name="Too-high Pareto $hat k$") +
+  # change scales to sth that makes sense
+  scale_x_continuous(trans = "pseudo_log", breaks = c(0, -500, -1000, -2000)) +
+  theme(
+    axis.text.y = element_blank(),
+    #axis.text.x = element_text(size=15)
+    legend.position = c(0.2,0.9)
   )
+
+plot_elpd_diff_intloo
+
+save_tikz_plot(plot = plot_elpd_diff_intloo, 
+               width = 6,
+               filename = here::here("case-studies", "epilepsy", "figures", "plot_elpd_diff_intloo.tex")
+)
+
 
 save_plot(here::here("case-studies", "epilepsy", "figures", "plot_elpd_diff_intloo.png"), 
           plot_elpd_diff_intloo, 
