ea1e5c4585e8ad8e6d3e832a6fd3bf89e05b3059
Anna Elisabeth Riha <e.riha@posteo.de>
2023-08-31 17:32:37 +0300

    clean-up, added function

--- case-studies/epilepsy/R/plot_posterior_trt_coeff_all.R
+++ case-studies/epilepsy/R/plot_posterior_trt_coeff_all.R
@@ -14,6 +14,7 @@ theme_set(theme_bw() +
 
 # load scripts
 source(here::here("case-studies", "epilepsy", "R", "save_tikz_plot.R"))
+source(here::here("case-studies", "epilepsy", "R", "get_plot_posterior_trt_coeffs.R"))
 
 # check and create dir if needed
 filedir = here::here("case-studies", "epilepsy", "figures")
@@ -21,9 +22,29 @@ if (!dir.exists(filedir)) {dir.create(filedir)}
 
 # load 
 df <- readr::read_rds(here::here("case-studies", "epilepsy", "results", "full_df_elpddiff_pbma.rds")) 
+models_combs_df <- readr::read_rds(here::here("case-studies", "epilepsy", "results", "models_combs_df.rds"))
 
-# posterior treatment effect for indistinguishable models with default PSIS-LOO-CV #### 
-plot_df_trt_default_all <- df |>
+# posterior treatment coefficients for all 24 models ####
+df_plot_reduced <- models_combs_df |>
+  filter(obs == "" & patient == "" & visit == "") |>
+  #mutate(high_pareto_ks = ifelse(n_high_pareto_ks > (NROW(brms::epilepsy) / 100) * 5, "yes", "no")) |>
+  select(draws_df, modelnames, family, model_id) |>
+  mutate(posterior_draws_trt = purrr::map(purrr::map(draws_df, pluck), "b_Trt1" )) |>
+  mutate(mean_post_trt = purrr::map_dbl(posterior_draws_trt, median)) |>
+  arrange(mean_post_trt) |>
+  mutate(model_id = forcats::fct_inorder(model_id)) |>
+  select(posterior_draws_trt, mean_post_trt, model_id, family) |>
+  unnest(posterior_draws_trt)
+
+plot_posterior_trt_coeff_all_reduced <- get_plot_posterior_trt_coeffs(df_plot_reduced) + 
+  theme(axis.text.y = element_text(size=9))
+
+save_tikz_plot(plot = plot_posterior_trt_coeff_all_reduced, 
+               width = 5.5,
+               filename = here::here("case-studies", "epilepsy", "figures", "plot_posterior_trt_coeff_all_reduced.tex"))
+
+# posterior treatment coefficients for all 192 models (default PSIS-LOO-CV)####
+df_plot_trt_default_all <- df |>
   mutate(modelnames = rownames_to_column(df)) |>
   filter(loo_computation == "default") |>
   mutate(high_pareto_ks = ifelse(n_high_pareto_ks > (NROW(brms::epilepsy) / 100) * 5, "yes", "no")) |>
@@ -37,17 +58,7 @@ plot_df_trt_default_all <- df |>
 
 readr::write_rds(plot_df_trt_default_all, here::here("case-studies", "epilepsy", "results", "plot_df_trt_default_all.rds"))
 
-# gradient interval plot of all models, color indicates computational issues ####
-plot_posterior_trt_default_all <- ggplot(plot_df_trt_default_all, aes(x = posterior_draws_trt, y = model_id, color = high_pareto_ks)) + 
-  stat_pointinterval(.width = c(.5, .95)) +
-  #xlim(-1.3, 0.8) + 
-  xlab("Coefficient for treatment") +
-  geom_vline(xintercept = 0) + 
-  scale_color_manual(values=c("yes" = "red", "no" = "black")) +  
-  theme(axis.text.y=element_text(size=4),
-        axis.title.y = element_blank(),
-        legend.position = "none")
-
+plot_posterior_trt_default_all <- get_plot_posterior_trt_coeffs(plot_df_trt_default_all)
 plot_posterior_trt_default_all
 
 save_tikz_plot(plot = plot_posterior_trt_default_all, 
