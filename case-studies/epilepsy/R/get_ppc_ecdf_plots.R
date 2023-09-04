6ce0cf908dc3950a518f560198f02650434c4435
Anna Elisabeth Riha <e.riha@posteo.de>
2023-09-04 17:11:59 +0300

    small adjustments to create a tikz of appropriate size

--- case-studies/epilepsy/R/get_ppc_ecdf_plots.R
+++ case-studies/epilepsy/R/get_ppc_ecdf_plots.R
@@ -16,7 +16,7 @@ options(mc.cores = nc)
 source(here::here("case-studies", "epilepsy", "R", "save_tikz_plot.R"))
 
 # load data
-df_reduced <- readr::read_rds()
+df_reduced <- readr::read_rds(here::here("case-studies", "epilepsy", "results", "epi_1_df_reduced.rds"))
   
 get_one_ecdf_overlay <- function(df, y, model_char = ""){
   # set ggplot theme
@@ -25,6 +25,9 @@ get_one_ecdf_overlay <- function(df, y, model_char = ""){
                     panel.grid.minor = element_blank(),
                     strip.background = element_blank(),
                     panel.background = element_blank()))
+  
+  # bayesplot colour scheme
+  bayesplot::color_scheme_set("gray")
   
   # get predictions for one model 
   yrep <- df |>
@@ -46,8 +49,11 @@ get_one_ecdf_overlay <- function(df, y, model_char = ""){
   modelname <- substr(modelname_long,1,regexpr(",",modelname_long)-1)
   
   # create plot
-  plot <- ppc_ecdf_overlay(y = y, yrep = yrep[[1]], discrete = TRUE) +
-    scale_x_continuous(limits=c(0,110)) +
+  plot <- ppc_ecdf_overlay(y = y, yrep = yrep[[1]][1:100,], discrete = TRUE) +
+    #scale_x_continuous(limits=c(0,110)) +
+    scale_x_continuous(trans="pseudo_log", 
+                       breaks=c(0, 5, 20, 50, 100), 
+                       limits=c(0,110)) +
     labs(subtitle = paste0(modelfamily)) +
     theme(legend.position="none")
   
@@ -56,11 +62,11 @@ get_one_ecdf_overlay <- function(df, y, model_char = ""){
 
 plot_ppc_ecdf_model_22 <- get_one_ecdf_overlay(df_reduced, brms::epilepsy$count, model_char = "Model 22")
 plot_ppc_ecdf_model_21 <- get_one_ecdf_overlay(df_reduced, brms::epilepsy$count, model_char = "Model 21")
-plot_ppc_ecdf_model_22_21 <- plot_ppc_ecdf_model_22 / plot_ppc_ecdf_model_21
+plot_ppc_ecdf_model_22_21 <- plot_ppc_ecdf_model_22 | plot_ppc_ecdf_model_21
 plot_ppc_ecdf_model_22_21
 
 save_tikz_plot(plot = plot_ppc_ecdf_model_22_21, 
-               width = 5.5,
+               width = 5,
                filename = here::here("case-studies", "epilepsy", "figures", "plot_ppc_ecdf_model_22_21.tex")
 )
 
