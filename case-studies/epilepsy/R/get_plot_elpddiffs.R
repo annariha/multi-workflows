10a3aa5eb203154da01a5b48bffb388095278cf3
Anna Elisabeth Riha <e.riha@posteo.de>
2023-09-04 17:14:12 +0300

    function for plotting elpddiffs of several models, still have to remove dependency on specific model families

---
+++ case-studies/epilepsy/R/get_plot_elpddiffs.R
@@ -0,0 +1,26 @@
+get_plot_elpddiffs <- function(df, subtitle_char = ""){
+  # set ggplot theme
+  theme_set(theme_bw() +
+              theme(panel.grid.major = element_blank(),
+                    panel.grid.minor = element_blank(),
+                    strip.background = element_blank(),
+                    panel.background = element_blank()))
+  
+  # prepare data for plotting 
+  df_plot <- df |>
+    arrange(elpd_diff) |>
+    mutate(model_id = forcats::fct_inorder(model_id)) |>
+    select(modelnames, family, elpd_diff, se_diff, n_high_pareto_ks, model_id, high_pareto_ks)
+  
+  # create plot 
+  plot_elpddiffs <- ggplot(data = df_plot, aes(elpd_diff, model_id, col = high_pareto_ks, shape = family)) +
+    geom_pointrange(aes(xmin=elpd_diff-se_diff, xmax=elpd_diff+se_diff)) +
+    geom_vline(xintercept = 0, linetype = "dashed", color = "gray") + 
+    labs(subtitle = subtitle_char) + 
+    xlab("$Delta widehat textrmelpd$") +
+    scale_color_manual(values=c("yes" = "red", "no" = "black")) + 
+    scale_shape_manual(values=c("poisson" = 1, "negbinomial" = 6)) +
+    theme(axis.title.y = element_blank(), legend.position = "none")
+  
+  return(plot_elpddiffs)
+}
