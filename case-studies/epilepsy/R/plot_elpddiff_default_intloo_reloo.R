850a894274478608eb226f37e8d0952f03b37e9b
Anna Elisabeth Riha <e.riha@posteo.de>
2023-10-11 19:18:20 +0300

    plot with plotting function, create four panel plot for elpddiff

--- case-studies/epilepsy/R/plot_elpddiff_default_intloo_reloo.R
+++ case-studies/epilepsy/R/plot_elpddiff_default_intloo_reloo.R
@@ -26,19 +26,31 @@ df_plot_elpddiff_default <- full_df_elpddiff_pbma |>
   mutate(model_id = forcats::fct_inorder(model_id)) |>
   select(modelnames, family, elpd_diff, se_diff, n_high_pareto_ks, model_id, high_pareto_ks, loo_computation)
 
-plot_elpddiff_all_default <- ggplot(data = df_plot_elpddiff_default, aes(elpd_diff, model_id, col = high_pareto_ks)) +
-  geom_pointrange(aes(xmin=elpd_diff-se_diff, xmax=elpd_diff+se_diff), shape=1, size=0.1) +
-  geom_vline(xintercept = 0, linetype = "dashed", color = "gray") + 
-  xlab("$Delta widehat textrmelpd$") +
-  ylab("Models") +
-  labs(subtitle = "Default PSIS-LOO-CV") +
-  scale_color_manual(values=c("yes" = "red", "no" = "black"), name="Too-high Pareto $hat k$") +
+plot_elpddiff_all_default <- get_plot_elpddiffs(df_plot_elpddiff_default, subtitle_char = "Default PSIS-LOO-CV: All models", ylabel_char = "Models") +
   scale_x_continuous(trans = "pseudo_log", 
                      breaks = c(0, -10,-100,-500,-1500),
                      limits = c(-1500,3)) +
   theme(axis.text.y = element_blank(), legend.position = "none")
 
-plot_elpddiff_all_default
+# create plot of models indistinguishable by elpd diff +/- se diff for default LOO ####
+df_plot_elpddiff_indist_default <- full_df_elpddiff_pbma |>
+  mutate(modelnames = rownames(full_df_elpddiff_pbma)) |>
+  filter(loo_computation == "default") |>
+  mutate(high_pareto_ks = ifelse(n_high_pareto_ks > 0, "yes", "no")) |>
+  filter(elpd_diff + se_diff >= 0) 
+
+plot_elpddiff_indist_default <- get_plot_elpddiffs(df_plot_elpddiff_indist_default, subtitle_char = "Indistinguishable models") +
+  theme(axis.title.y = element_blank(), legend.position = "none")
+
+# Combine elpddiff for all and indist. models for default PSIS-LOO-CV ####
+plot_elpddiff_all_indist_default <- plot_elpddiff_all_default | plot_elpddiff_indist_default
+plot_elpddiff_all_indist_default
+
+save_tikz_plot(plot = plot_elpddiff_all_indist_default, 
+               width = 6.5,
+               height = 5,
+               filename = here::here("case-studies", "epilepsy", "figures", "plot_elpddiff_all_indist_default.tex")
+)
 
 # create plot of elpd diff +/- se diff for integrated LOO + reloo() ####
 df_plot_elpddiff_intloo_reloo <- full_df_intloo_reloo |>
@@ -48,24 +60,45 @@ df_plot_elpddiff_intloo_reloo <- full_df_intloo_reloo |>
   mutate(model_id = forcats::fct_inorder(model_id)) |>
   select(modelnames, family, elpd_diff, se_diff, n_high_pareto_ks, model_id, high_pareto_ks, loo_computation)
 
-plot_elpddiff_all_intloo_reloo <- ggplot(data = df_plot_elpddiff_intloo_reloo, aes(elpd_diff, model_id, col = high_pareto_ks)) +
-  geom_pointrange(aes(xmin=elpd_diff-se_diff, xmax=elpd_diff+se_diff), shape=1, size=0.1) +
-  geom_vline(xintercept = 0, linetype = "dashed", color = "gray") + 
-  xlab("$Delta widehat textrmelpd$") +
-  labs(subtitle = "Integrated PSIS-LOO-CV and exact LOO-CV") + 
-  scale_color_manual(values=c("yes" = "red", "no" = "black"),  name="Too-high Pareto $hat k$") +
+plot_elpddiff_all_intloo_reloo <- get_plot_elpddiffs(df_plot_elpddiff_intloo_reloo, subtitle_char = "Integrated PSIS-LOO-CV + exact LOO-CV: All models", ylabel_char = "Models") +
   scale_x_continuous(trans = "pseudo_log", 
                      breaks = c(0, -10,-100,-500,-1500),
                      limits = c(-1500,3)) +
-  theme(
-    axis.text.y = element_blank(),
-    axis.title.y = element_blank(),
-    axis.ticks.y = element_blank(),
-    legend.position = "none"
-  )
+  theme(axis.text.y = element_blank(), legend.position = "none")
 
 plot_elpddiff_all_intloo_reloo
 
+# create plot of elpd diff +/- se diff for integrated LOO and reloo() ####
+df_plot_elpddiff_indist_intloo_reloo <- full_df_intloo_reloo |>
+  mutate(modelnames = rownames(full_df_intloo_reloo)) |>
+  mutate(high_pareto_ks = ifelse(n_high_pareto_ks > 0, "yes", "no")) |>
+  filter(elpd_diff + se_diff >= 0) 
+
+plot_elpddiff_indist_intloo_reloo <- get_plot_elpddiffs(df_plot_elpddiff_indist_intloo_reloo, subtitle_char = "Indistinguishable models") +
+  theme(axis.title.y = element_blank(), legend.position = "none")
+
+plot_elpddiff_indist_intloo_reloo
+
+# Combine elpddiff for all and indist. models for for integrated LOO and reloo() ####
+plot_elpddiff_all_indist_intloo_reloo <- plot_elpddiff_all_intloo_reloo | plot_elpddiff_indist_intloo_reloo
+plot_elpddiff_all_indist_intloo_reloo
+
+save_tikz_plot(plot = plot_elpddiff_all_indist_intloo_reloo, 
+               width = 6.5,
+               height = 5,
+               filename = here::here("case-studies", "epilepsy", "figures", "plot_elpddiff_all_indist_intloo_reloo.tex")
+)
+
+# Combine all four plots ####
+plot_elpddiff_combined_default_intloo_reloo <- (plot_elpddiff_all_default | plot_elpddiff_indist_default) / (plot_elpddiff_all_intloo_reloo | plot_elpddiff_indist_intloo_reloo)
+plot_elpddiff_combined_default_intloo_reloo
+
+save_tikz_plot(plot = plot_elpddiff_combined_default_intloo_reloo, 
+               width = 6.5,
+               height = 7,
+               filename = here::here("case-studies", "epilepsy", "figures", "plot_elpddiff_combined_default_intloo_reloo.tex"))
+
+# Combine plots for all models for the two different estimation methods ####
 plot_elpddiff_all_default_reloo <- plot_elpddiff_all_default | plot_elpddiff_all_intloo_reloo
 plot_elpddiff_all_default_reloo
 
@@ -75,15 +108,14 @@ save_tikz_plot(plot = plot_elpddiff_all_default_reloo,
                filename = here::here("case-studies", "epilepsy", "figures", "plot_elpddiff_all_default_reloo.tex")
 )
 
-# create plot of models indistinguishable by elpd diff +/- se diff for default LOO ####
-df_plot_elpddiff_default <- full_df_elpddiff_pbma |>
-  mutate(modelnames = rownames(full_df_elpddiff_pbma)) |>
-  filter(loo_computation == "default") |>
-  mutate(high_pareto_ks = ifelse(n_high_pareto_ks > 0, "yes", "no")) |>
-  filter(elpd_diff + se_diff >= 0) 
+# Combine plots for indistinguishabl models for the two different estimation methods ####
+plot_elpddiff_indist_reloo <- plot_elpddiff_indist_default | plot_elpddiff_indist_intloo_reloo
+plot_elpddiff_indist_reloo
 
-plot_elpddiff_default <- get_plot_elpddiffs(df_plot_elpddiff_default)
-plot_elpddiff_default
+save_tikz_plot(plot = plot_elpddiff_indist_reloo, 
+               width = 5.5,
+               filename = here::here("case-studies", "epilepsy", "figures", "plot_elpddiff_indist_reloo.tex")
+)
 
 # create plot of elpd diff +/- se diff for integrated LOO ####
 df_plot_elpddiff_intloo <- full_df_elpddiff_pbma |>
@@ -95,24 +127,7 @@ df_plot_elpddiff_intloo <- full_df_elpddiff_pbma |>
 plot_elpddiff_intloo <- get_plot_elpddiffs(df_plot_elpddiff_intloo)
 plot_elpddiff_intloo
 
-# create plot of elpd diff +/- se diff for integrated LOO and reloo() ####
-df_plot_elpddiff_intloo_reloo <- full_df_intloo_reloo |>
-  mutate(modelnames = rownames(full_df_intloo_reloo)) |>
-  mutate(high_pareto_ks = ifelse(n_high_pareto_ks > 0, "yes", "no")) |>
-  filter(elpd_diff + se_diff >= 0) 
-
-plot_elpddiff_intloo_reloo <- get_plot_elpddiffs(df_plot_elpddiff_intloo_reloo)
-plot_elpddiff_intloo_reloo
-
-# combine plots and save ####
-plot_elpddiff_indist_reloo <- plot_elpddiff_default | plot_elpddiff_intloo_reloo
-plot_elpddiff_indist_reloo
-
-save_tikz_plot(plot = plot_elpddiff_indist_reloo, 
-               width = 5.5,
-               filename = here::here("case-studies", "epilepsy", "figures", "plot_elpddiff_indist_reloo.tex")
-)
-
+# Combine all three plots for indistinguishable models for default, integrated and integrated+exact ####
 plot_elpddiff_indist_default_intloo_reloo <-  plot_elpddiff_default | plot_elpddiff_intloo | plot_elpddiff_intloo_reloo
 plot_elpddiff_indist_default_intloo_reloo
 
