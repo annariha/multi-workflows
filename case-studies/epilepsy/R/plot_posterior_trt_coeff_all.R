caf41bf5c14ba18c2e22f1bed8f5a99d7680c118
Anna Elisabeth Riha <e.riha@posteo.de>
2023-10-19 19:56:05 +0300

    typo, new file location

--- case-studies/epilepsy/R/plot_posterior_trt_coeff_all.R
+++ case-studies/epilepsy/R/plot_posterior_trt_coeff_all.R
@@ -20,21 +20,19 @@ models_combs_df <- readr::read_rds(here::here("case-studies", "epilepsy", "resul
 # posterior treatment coefficients for all 24 models ####
 df_plot_reduced <- models_combs_df |>
   filter(obs == "" & patient == "" & visit == "") |>
-  #mutate(high_pareto_ks = ifelse(n_high_pareto_ks > (NROW(brms::epilepsy) / 100) * 5, "yes", "no")) |>
   select(draws_df, modelnames, family, model_id) |>
   mutate(posterior_draws_trt = purrr::map(purrr::map(draws_df, pluck), "b_Trt1" )) |>
-  mutate(mean_post_trt = purrr::map_dbl(posterior_draws_trt, median)) |>
-  arrange(mean_post_trt) |>
+  mutate(median_post_trt = purrr::map_dbl(posterior_draws_trt, median)) |>
+  arrange(median_post_trt) |>
   mutate(model_id = forcats::fct_inorder(model_id)) |>
-  select(posterior_draws_trt, mean_post_trt, model_id, family) |>
+  select(posterior_draws_trt, median_post_trt, model_id, family) |>
   unnest(posterior_draws_trt)
 
-plot_posterior_trt_coeff_all_reduced <- get_plot_posterior_trt_coeffs(df_plot_reduced) + 
-  theme(axis.text.y = element_text(size=9))
+plot_posterior_trt_coeff_all_reduced <- get_plot_posterior_trt_coeffs(df_plot_reduced)
 
 save_tikz_plot(plot = plot_posterior_trt_coeff_all_reduced, 
-               width = 5.5,
-               filename = here::here("case-studies", "epilepsy", "figures", "plot_posterior_trt_coeff_all_reduced.tex"))
+               width = 5,
+               filename = here::here("case-studies", "epilepsy", "figures", "epi-1", "plot_posterior_trt_coeff_all_reduced.tex"))
 
 # posterior treatment coefficients for all 192 models (default PSIS-LOO-CV)####
 df_plot_trt_default_all <- df |>
@@ -51,10 +49,10 @@ df_plot_trt_default_all <- df |>
 
 readr::write_rds(df_plot_trt_default_all, here::here("case-studies", "epilepsy", "results", "plot_df_trt_default_all.rds"))
 
-plot_posterior_trt_default_all <- get_plot_posterior_trt_coeffs(df_plot_trt_default_all)
+plot_posterior_trt_default_all <- get_plot_posterior_trt_coeffs(df_plot_trt_default_all, ytextsize = 4)
 plot_posterior_trt_default_all
 
 save_tikz_plot(plot = plot_posterior_trt_default_all, 
                width = 6.5,
                height = 8.5,
-               filename = here::here("case-studies", "epilepsy", "figures", "plot_posterior_trt_coeff_all.tex"))
+               filename = here::here("case-studies", "epilepsy", "figures", "epi-2", "plot_posterior_trt_coeff_all.tex"))
