f736b716d8a50a12b094639d34156410e6ebb519
Anna Elisabeth Riha <e.riha@posteo.de>
2023-12-14 21:04:29 +0200

    new theme, add point-wise elpddiff plot

--- case-studies/epilepsy/R/epi-2/inspect_pointwise_elpds.R
+++ case-studies/epilepsy/R/epi-2/inspect_pointwise_elpds.R
@@ -5,8 +5,11 @@
 if(!requireNamespace("pacman")) install.packages("pacman")
 pacman::p_load(here, tictoc, tidyverse, posterior, ggplot2, patchwork)
 
+# set seed
+set.seed(42424242)
+
 # set ggplot theme
-theme_set(theme_bw() +
+theme_set(theme_classic() +
             theme(panel.grid.major = element_blank(),
                   panel.grid.minor = element_blank(),
                   strip.background = element_blank(),
@@ -16,24 +19,23 @@ theme_set(theme_bw() +
                   axis.title = element_text(size=7),
                   axis.text = element_text(size=7)))
 
+# helper functions 
+source(here::here("case-studies", "epilepsy", "R", "get_plot_elpddiffs.R"))
+source(here::here("case-studies", "epilepsy", "R", "save_tikz_plot.R"))
+
 # get loo-objects, comparisons, model fits ####
 
 # load comparison df
-full_df_intloo_reloo <- readr::read_rds(here::here("case-studies", "epilepsy", "results", "epi-2", "full_df_elpddiff_loobb_intloo_reloo.rds"))
+full_comparisons_df_wo_intloo_reloo <- readr::read_rds(here::here("case-studies", "epilepsy", "results", "epi-2", "full_comparisons_df_wo_intloo_reloo.rds"))
 
 # What are the models we would consider indistinguishable just based on elpd diff & se?
-df_indist <- full_df_intloo_reloo |>
-  mutate(modelnames = rownames(full_df_intloo_reloo)) |>
+df_indist <- full_comparisons_df_wo_intloo_reloo |>
+  mutate(modelnames = rownames(full_comparisons_df_wo_intloo_reloo)) |>
   select(modelnames, family, elpd_diff, se_diff, loo_bb_weight, n_high_pareto_ks, model_id) |>
   filter(elpd_diff + 2*se_diff >= 0)
 
 # free up some space
-rm(full_df_intloo_reloo)
-
-# What is the best model?
-best_model_name <- df_indist |>
-  filter(elpd_diff == 0) |>
-  pull(modelnames)
+#rm(full_comparisons_df_wo_intloo_reloo)
 
 # load loo object with default + integrated PSIS-LOO-CV and reloo()
 loos_intloo_reloo <- readr::read_rds(here::here("case-studies", "epilepsy", "results", "loos_intloo_reloo.rds"))
@@ -51,22 +53,167 @@ get_pointwise_elpds <- function(x, loo_objects){
   return(pointwise_elpds)
 }
 
+# What is the best model?
+best_model_name <- df_indist |>
+  filter(elpd_diff == 0) |>
+  pull(modelnames)
+
 # point-wise elpds for the best model 
 pointwise_elpds_best <- get_pointwise_elpds(best_model_name, loos_intloo_reloo)
+# point-wise pareto-smoothed elpds for best model
+ps_pointwise_elpds_best <- pareto_smooth(pointwise_elpds_best, r_eff = 1, return_k = FALSE)
+
+################################################################################
+qplot(ps_pointwise_elpds_best, pointwise_elpds_best) + geom_abline()
+
+ggplot(data=NULL, aes(x=pointwise_elpds_best)) + 
+  geom_histogram(bins=263)
+
+ggplot(data=NULL, aes(x=ps_pointwise_elpds_best)) + 
+  geom_histogram(bins=263)
+
+mean(pointwise_elpds_best)
+mean(ps_pointwise_elpds_best)
+
+pareto_diags(ps_pointwise_elpds_best)
+################################################################################
 
 # point-wise elpds & point-wise differences in elpd compared to the best model 
 loos_pointwise_indist_models <- df_indist |>
-  select(elpd_diff, se_diff, modelnames, model_id) |>
   mutate(pointwise_elpds = purrr::map(modelnames, ~get_pointwise_elpds(.x, loo_objects = loos_intloo_reloo))) |>
   mutate(pointwise_elpd_diffs = purrr::map(pointwise_elpds, function(x) x - pointwise_elpds_best)) |>
-  mutate(pareto_smoothed_pointwise_elpds = purrr::map(pointwise_elpds, ~pareto_smooth(.x, r_eff = 1, return_k = FALSE)))
+  mutate(ps_pointwise_elpds = purrr::map(pointwise_elpds, ~pareto_smooth(.x, r_eff = 1, return_k = FALSE))) |> 
+  mutate(ps_pointwise_elpd_diffs = purrr::map(ps_pointwise_elpds, function(x) x - ps_pointwise_elpds_best))
+
+# Which models have few extreme values in point-wise elpds? Those might drive their large standard errors of the difference in elpd. 
+
+# check: elpd_diff is obtained via sum of point-wise values or (equivalently) mean of the point-wise values multiplied with the number of obs
+loos_pointwise_indist_models |> 
+  mutate(mean_elpd_diff_check1 = purrr::map_dbl(pointwise_elpd_diffs, function(x) sum(x)), 
+         mean_elpd_diff_check2 = purrr::map_dbl(pointwise_elpd_diffs, function(x) mean(x) * NROW(x))) |> 
+  select(elpd_diff, mean_elpd_diff_check1, mean_elpd_diff_check2)
+
+# Given these point-wise values, can we reliably obtain estimates for mean elpd? 
+# Is the normal approximation valid? 
+
+# add Pareto diagnostics 
+loos_pointwise_indist_models <- loos_pointwise_indist_models |>
+  mutate(min_ss_elpd = purrr::map_dbl(purrr::map_dbl(purrr::map(pointwise_elpds, ~pareto_diags(.x, r_eff = 1)), "min_ss"), ~round(.x, digits = 2))) |>
+  mutate(khat_elpd = purrr::map_dbl(purrr::map(pointwise_elpds, ~pareto_diags(.x, r_eff = 1)), "khat")) |>
+  mutate(khat_threshold_elpd = purrr::map_dbl(purrr::map(pointwise_elpds, ~pareto_diags(.x, r_eff = 1)), "khat_threshold")) |>
+  mutate(min_ss_ps_elpd = purrr::map_dbl(purrr::map_dbl(purrr::map(ps_pointwise_elpds, ~pareto_diags(.x, r_eff = 1)), "min_ss"), ~round(.x, digits = 2))) |>
+  mutate(khat_ps_elpd = purrr::map_dbl(purrr::map(ps_pointwise_elpds, ~pareto_diags(.x, r_eff = 1)), "khat")) |>
+  mutate(khat_threshold_ps_elpd = purrr::map_dbl(purrr::map(ps_pointwise_elpds, ~pareto_diags(.x, r_eff = 1)), "khat_threshold"))
+
+# store row of best model 
+best_model_row <- loos_pointwise_indist_models[loos_pointwise_indist_models$modelnames == best_model_name,] |>
+  mutate(min_ss_elpd_diff = NA,
+         khat_elpd_diff = NA,
+         khat_threshold_elpd_diff = NA,
+         min_ss_ps_elpd_diff = NA)
+
+# add Pareto diagnostics of differences (excluding the best model)  
+loos_pointwise_indist_models_temp <- loos_pointwise_indist_models |>
+  filter(modelnames != best_model_name) |>
+  mutate(min_ss_elpd_diff = purrr::map_dbl(purrr::map(pointwise_elpd_diffs, ~pareto_diags(.x, r_eff = 1)), "min_ss")) |>
+  mutate(khat_elpd_diff = purrr::map_dbl(purrr::map(pointwise_elpd_diffs, ~pareto_diags(.x, r_eff = 1)), "khat")) |>
+  mutate(khat_threshold_elpd_diff = purrr::map_dbl(purrr::map(pointwise_elpd_diffs, ~pareto_diags(.x, r_eff = 1)), "khat_threshold")) |>
+  mutate(min_ss_ps_elpd_diff = purrr::map_dbl(purrr::map(ps_pointwise_elpd_diffs, ~pareto_diags(.x, r_eff = 1)), "min_ss")) 
+
+# add best model back in 
+loos_pointwise_indist_models <- loos_pointwise_indist_models_temp |>
+  rbind(best_model_row) |>
+  mutate(min_ss_elpd_diff = purrr::map_dbl(min_ss_elpd_diff, ~round(.x, digits = 2))) |>
+  mutate(min_ss_ps_elpd_diff = purrr::map_dbl(min_ss_ps_elpd_diff, ~round(.x, digits = 2)))
+
+# plot: point-wise difference for worst model among the 86 models vs elpddiff = 0 ####
+plot_df_worst_models <- loos_pointwise_indist_models |>
+  filter(elpd_diff < -100) |>
+  unnest(cols = c(pointwise_elpds, pointwise_elpd_diffs, ps_pointwise_elpds, ps_pointwise_elpd_diffs)) |>
+  group_by(model_id) |>
+  mutate(obs_id = row_number())
+
+# get ids of extreme observations
+outlier_ids <- brms::epilepsy |>
+  filter(count >= mean(count)+4*sd(count)) |>
+  pull(obs)
+
+# extract the breaks 
+# pretty_breaks <- pretty(plot_df_worst_model$obs_id)
+
+plot_elpddiff_pointwise_worst_models <- ggplot(plot_df_worst_models, aes(x = obs_id, y=pointwise_elpd_diffs, colour=model_id)) + 
+  geom_point() + 
+  geom_hline(yintercept = 0) + 
+  geom_vline(xintercept = as.numeric(outlier_ids), linetype = "dotted") + 
+  scale_x_continuous(breaks = as.numeric(outlier_ids), 
+                     labels = as.numeric(outlier_ids)) +
+  scale_colour_grey(start = .9, end = 0) +
+  xlab("Observation ID") +
+  ylab("point-wise difference in elpd") +
+  theme(legend.title = element_blank())
+
+save_tikz_plot(plot = plot_elpddiff_pointwise_worst_models, 
+               width = 5.5,
+               filename = here::here("case-studies", "epilepsy", "figures", "epi-2", "plot_extended_elpddiff_pointwise_worst_models.tex"))
+
+################################################################################
+# Does Pareto-smoothing help? 
+length(which(loos_pointwise_indist_models$min_ss_elpd > NROW(brms::epilepsy)))
+length(which(loos_pointwise_indist_models$min_ss_ps_elpd > NROW(brms::epilepsy)))
+length(which(loos_pointwise_indist_models$min_ss_elpd_diff > NROW(brms::epilepsy)))
+length(which(loos_pointwise_indist_models$min_ss_ps_elpd_diff > NROW(brms::epilepsy)))
+################################################################################
+
+# check for which models we can obtain reliable estimates for the mean and the normal approx. holds for the mean elpd diff based on pointwise elpds
+# models where khat > threshold and min_ss > NROW(brms::epilepsy) for the difference in elpd
+loos_problematic_models <- loos_pointwise_indist_models |> 
+  filter(khat_elpd_diff > khat_threshold_elpd_diff & min_ss_elpd_diff > NROW(brms::epilepsy)) |> 
+  select(model_id, modelnames, elpd_diff, se_diff)
+
+# extract model names for problematic models ####
+modelnames_problematic_models <- loos_pointwise_indist_models |> 
+  filter(khat_elpd_diff > khat_threshold_elpd_diff & min_ss_elpd_diff > NROW(brms::epilepsy)) |> 
+  pull(modelnames)
+
+model_id_problematic_models <- loos_pointwise_indist_models |> 
+  filter(khat_elpd_diff > khat_threshold_elpd_diff & min_ss_elpd_diff > NROW(brms::epilepsy)) |> 
+  pull(model_id)
+
+readr::write_rds(model_id_problematic_models, here::here("case-studies", "epilepsy", "results", "epi-2", "model_id_problematic_models.rds"))
+
+# plot: elpd difference for filtered models ####
+df_plot_elpddiff_filtered <- loos_pointwise_indist_models |>
+  filter(!modelnames %in% modelnames_problematic_models) 
+
+plot_elpddiff_filtered <- get_plot_elpddiffs(df_plot_elpddiff_filtered, pointsize = 2) +
+  theme(axis.title.y = element_blank(), axis.text.y = element_text(size=6), legend.position = "none")
+
+plot_elpddiff_filtered
+
+# focus on models with low elpd diff
+modelnames_large_se <- loos_pointwise_indist_models |>
+  filter(elpd_diff < -30) |>
+  pull(modelnames)
+
+# check whether these models are also problematic wrt khat > threshold and min_ss > NROW(brms::epilepsy) for the difference in elpd
+which(modelnames_large_se %in% modelnames_problematic_models)
+
+df_plot_elpddiff_large_se <- loos_pointwise_indist_models |>
+  filter(!modelnames %in% modelnames_large_se) 
+
+plot_elpddiff_large_se <- get_plot_elpddiffs(df_plot_elpddiff_large_se) +
+  theme(axis.title.y = element_blank(), axis.text.y = element_text(size=5), legend.position = "none")
+
+plot_elpddiff_large_se
+
+################################################################################
 
 # df for plotting 
 plot_df <- loos_pointwise_indist_models |>
   # filter for best models and worst models 
-  filter(elpd_diff == 0 | elpd_diff < (-50)) |>
+  #filter(elpd_diff == 0 | elpd_diff < (-50)) |>
   #select(model_id, modelnames, pointwise_elpds, pointwise_elpd_diffs) |>
-  unnest(cols = c(pointwise_elpds, pointwise_elpd_diffs)) |>
+  unnest(cols = c(pointwise_elpds, pointwise_elpd_diffs, ps_pointwise_elpds, ps_pointwise_elpd_diffs)) |>
   group_by(model_id) |>
   mutate(obs_id = row_number())
 
@@ -84,6 +231,12 @@ ggplot(plot_df, aes(x = obs_id, y=pointwise_elpds, group = model_id, color = mod
   scale_x_continuous(breaks = c(pretty_br, as.numeric(outlier_ids)), 
                      labels = c(pretty_br, as.numeric(outlier_ids)))
 
+ggplot(plot_df, aes(x = obs_id, y=ps_pointwise_elpds, group = model_id, color = model_id)) + 
+  geom_point() + 
+  geom_vline(xintercept = as.numeric(outlier_ids), linetype = "dotted") + 
+  scale_x_continuous(breaks = c(pretty_br, as.numeric(outlier_ids)), 
+                     labels = c(pretty_br, as.numeric(outlier_ids)))
+
 ggplot(plot_df, aes(x = obs_id, y=pointwise_elpd_diffs, group = model_id, color = model_id)) + 
   geom_point() + 
   geom_hline(yintercept = 0) + 
@@ -91,8 +244,13 @@ ggplot(plot_df, aes(x = obs_id, y=pointwise_elpd_diffs, group = model_id, color 
   scale_x_continuous(breaks = c(pretty_br, as.numeric(outlier_ids)), 
                      labels = c(pretty_br, as.numeric(outlier_ids)))
 
-
-# check whether we can obtain reliable estimates for the mean and the normal approx. holds for the pointwise elpds
+ggplot(plot_df, aes(x = obs_id, y=ps_pointwise_elpd_diffs, group = model_id, color = model_id)) + 
+  geom_point() + 
+  geom_hline(yintercept = 0) + 
+  geom_vline(xintercept = as.numeric(outlier_ids), linetype = "dotted") + 
+  scale_x_continuous(breaks = c(pretty_br, as.numeric(outlier_ids)), 
+                     labels = c(pretty_br, as.numeric(outlier_ids)))
+################################################################################
 
 # point-wise elpds for the best model 
 pointwise_elpds_best <- plot_df |> 
